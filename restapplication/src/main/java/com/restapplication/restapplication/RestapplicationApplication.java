d3397e0930704b07eb35d50ed2641bfc411b9215
hashim-mjp <m.hashimb4u@gmail.com>
2018-11-21 17:04:18 +0530

    feature-app-01 : Project setup

---
+++ restapplication/src/main/java/com/restapplication/restapplication/RestapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.restapplication.restapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapplicationApplication.class, args);
+	}
+}
